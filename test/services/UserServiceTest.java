12f6701567dba0d2a9ba924151c5ab18196b3b9b
Hauxii <haukur91@gmail.com>
2016-10-25 13:07:11 +0000

    added routes and tests

---
+++ test/services/UserServiceTest.java
@@ -0,0 +1,38 @@
+package services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenny on 25.10.2016.
+ */
+public class UserServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void updateProfile() throws Exception {
+
+    }
+
+    @Test
+    public void addFavoriteVideo() throws Exception {
+
+    }
+
+    @Test
+    public void addCloseFriend() throws Exception {
+
+    }
+
+}
---
+++ test/services/VideoServiceTest.java
@@ -0,0 +1,38 @@
+package services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenny on 25.10.2016.
+ */
+public class VideoServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void addVideoToChannelAllVideos() throws Exception {
+
+    }
+
+    @Test
+    public void addVideoToChannel() throws Exception {
+
+    }
+
+    @Test
+    public void removeVideo() throws Exception {
+
+    }
+
+}
